8b7079cd9c8caca2109b9a43fee69c232116056b
Guillem Pascual <guillempascual@serplus.es>
2016-02-28 09:29:23 +0100

    add Smarty Templating

--- src/Home/Controller/Customer.php
+++ src/Home/Controller/Customer.php
@@ -4,24 +4,38 @@ namespace Mpwarapp\Home\Controller;
 
 use Mpwarfw\Component\Response\ResponseHTTP;
 use Mpwarfw\Component\Response\ResponseJSON;
+use Mpwarfw\Component\Template\SmartyTemplate;
 use Mpwarfw\Component\Template\TwigTemplate;
 
 class Customer extends DefaultController
 {
     public function show()
     {
-        $result = ['vino rioja' => ['s'=>'ffff','d'=>'kjdsffdj']];
+        $result = ['vino rioja' => ['s' => 'ffff', 'd' => 'kjdsffdj']];
 
         $response = new ResponseJSON();
         $response->setResponse($result);
         return $response;
     }
-        public function show_all()
+
+    public function show_all()
     {
         $all_customers = array('Alex', 'Guillem', 'Heidi', 'María', 'Laura');
 
         $templating = new TwigTemplate(self::VIEW_PATH);
-        $html = $templating->createView('showAll',array('all_customers' => $all_customers));
+        $html = $templating->createView('showAll', array('title' => "All Customers", 'all_customers' => $all_customers));
+
+        $response = new ResponseHTTP();
+        $response->setResponse($html);
+        return $response;
+    }
+
+    public function show_vip_all()
+    {
+        $all_customers = array('Alex', 'Guillem', 'Heidi', 'María', 'Laura');
+
+        $templating = new SmartyTemplate(self::VIEW_PATH);
+        $html = $templating->createView('showAll', array('title' => "VIP Customers", 'all_customers' => $all_customers));
 
         $response = new ResponseHTTP();
         $response->setResponse($html);
---
+++ src/Home/View/showAll.tpl
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>{$title}</title>
+</head>
+<body>
+{foreach $all_customers as $customer}
+    <h2>{$customer}</h2>
+{/foreach}
+</body>
+</html>
