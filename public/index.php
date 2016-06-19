3aa8038969d269ebd5212b6642fa701c7daba7b8
Guillem Pascual <guillempascualaventin@gmail.com>
2016-06-19 12:56:36 +0200

    Cleaning index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(-1);
-ini_set("display_errors", "true");
-
 use Mpwarfw\Component\Bootstrap\Bootstrap;
 use Mpwarfw\Component\Request\Request;
 
--- public/index_dev.php
+++ public/index_dev.php
@@ -9,7 +9,6 @@ use Mpwarfw\Component\Request\Request;
 require_once  __DIR__ . '/../vendor/autoload.php';
 require_once  __DIR__ . '/../app/constants.php';
 
-
 $parser = new \Symfony\Component\Yaml\Parser();
 $bootstrap = new Bootstrap($parser,APP_PATH,'DEV');
 
